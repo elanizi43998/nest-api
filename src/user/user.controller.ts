19bfbafdabd3a540dfce4565e618f04dc3a2343f
elanzi43998 <makaveli.2017dogg@gmail.com>
2022-04-14 15:39:14 +0000

    Costum param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,18 @@
 import { ConsoleLogger, Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JWTGuard } from 'src/auth/guard/jwt.guard';
 
+@UseGuards(JWTGuard)
 @Controller('user')
 export class UserController {
-  @UseGuards(JWTGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-      console.log({
-          user: req.user
-      })
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('id') userId: number) {
+    console.log({
+      userId,
+    });
+    return user;
   }
 }
